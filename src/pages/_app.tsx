144be93682eed3d93ac7b74f3606d335b410a884
codemechanica <code.mechanica@gmail.com>
2024-12-30 22:29:06 +0900

    added new console.log code to _app.tsx but doesn't seem to be working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,39 @@
-import { useMemo } from 'react';
+// import { useMemo } from 'react';
+// import type { AppProps } from 'next/app';
+// import {
+//   createIsographEnvironment,
+//   createIsographStore,
+//   IsographEnvironmentProvider,
+// } from '@isograph/react';
+
+// function makeNetworkRequest<T>(queryText: string, variables: any): Promise<T> {
+//   let promise = fetch(
+//     'https://swapi-graphql.netlify.app/.netlify/functions/index',
+//     {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({ query: queryText, variables }),
+//     },
+//   ).then((response) => response.json());
+
+//   return promise;
+// }
+
+// export default function App({ Component, pageProps }: AppProps) {
+//   const environment = useMemo(
+//     () => createIsographEnvironment(createIsographStore(), makeNetworkRequest),
+//     [],
+//   );
+//   return (
+//     <IsographEnvironmentProvider environment={environment}>
+//       <Component {...pageProps} />
+//     </IsographEnvironmentProvider>
+//   );
+// }
+
+import { useMemo, Suspense } from 'react';
 import type { AppProps } from 'next/app';
 import {
   createIsographEnvironment,
@@ -6,8 +41,11 @@ import {
   IsographEnvironmentProvider,
 } from '@isograph/react';
 
-function makeNetworkRequest<T>(queryText: string, variables: any): Promise<T> {
-  let promise = fetch(
+function makeNetworkRequest<T>(
+  queryText: string,
+  variables: unknown,
+): Promise<T> {
+  const promise = fetch(
     'https://swapi-graphql.netlify.app/.netlify/functions/index',
     {
       method: 'POST',
@@ -16,18 +54,39 @@ function makeNetworkRequest<T>(queryText: string, variables: any): Promise<T> {
       },
       body: JSON.stringify({ query: queryText, variables }),
     },
-  ).then((response) => response.json());
+  ).then(async (response) => {
+    const json = await response.json();
+    console.log(json)
+
+    if (response.ok) {
+      return json;
+    } else {
+      throw new Error('NetworkError', {
+        cause: json,
+      });
+    }
+  });
   return promise;
 }
 
-export default function App({ Component, pageProps }: AppProps) {
-  const environment = useMemo(
-    () => createIsographEnvironment(createIsographStore(), makeNetworkRequest),
-    [],
-  );
-  return (
-    <IsographEnvironmentProvider environment={environment}>
-      <Component {...pageProps} />
-    </IsographEnvironmentProvider>
-  );
-}
+// export default function App({ Component, pageProps }: AppProps) {
+//   const environment = useMemo(
+//     () =>
+//       createIsographEnvironment(
+//         createIsographStore(),
+//         makeNetworkRequest,
+//         // Optional missing field handler
+//         null,
+//         // Optional logger
+//         console.log(),
+//       ),
+//     [],
+//   );
+//   return (
+//     <IsographEnvironmentProvider environment={environment}>
+//       <Suspense fallback="loading">
+//         <Component {...pageProps} />
+//       </Suspense>
+//     </IsographEnvironmentProvider>
+//   );
+// }
